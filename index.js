f01e08d1f621cf9cc1fff29444ab62fc0c3e418f
MubeenSikandar <68024800+MubeenSikandar@users.noreply.github.com>
2023-01-27 18:21:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -33,10 +33,12 @@ app.get('/', async (req, res) => {
   });
 });
 
+const PORT = process.env.PORT || 8080;
+
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
-    app.listen(8080, () => console.log('Server started on port 8080'));
+    app.listen(`${PORT}`, () => console.log(`Server started on port ${PORT}`));
   } catch (error) {
     console.log(error);
   }
